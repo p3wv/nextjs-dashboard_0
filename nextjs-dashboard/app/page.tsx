e6c4eb0169a36c805aafd232d98e45b006c8b4d8
p3wv <memiarzyskrzynia@gmail.com>
2024-11-22 12:35:19 +0100

    chapter 15 DONE (on to adding metadata to the page)

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -5,6 +5,8 @@ import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
+export const experimental_ppr = true
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
